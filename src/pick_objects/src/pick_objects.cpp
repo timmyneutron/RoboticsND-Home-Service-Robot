50c8d92b464e999b50fcd357de0b1ad6eeb49a8d
Tim Abrahamsen <timmy_neutron@mac.com>
2019-04-25 19:47:30 +0000

    feat: Add pick_objects node
    Add pick_objects node and pick_objects.sh script to send
    commands to the robot

---
+++ src/pick_objects/src/pick_objects.cpp
@@ -0,0 +1,50 @@
+#include <ros/ros.h>
+#include <move_base_msgs/MoveBaseAction.h>
+#include <actionlib/client/simple_action_client.h>
+
+typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
+
+int main(int argc, char** argv) {
+  ros::init(argc, argv, "pick_objects");
+  
+  MoveBaseClient ac("move_base", true);
+  
+  while(!ac.waitForServer(ros::Duration(5.0))) {
+    ROS_INFO("Waiting for the move_base action server to come up");
+  }
+  
+  move_base_msgs::MoveBaseGoal goal;
+  
+  goal.target_pose.header.frame_id = "map";
+  goal.target_pose.header.stamp = ros::Time::now();
+
+  goal.target_pose.pose.position.y = 1.0;
+  goal.target_pose.pose.orientation.w = 1.0;
+  
+  ROS_INFO("Sending goal");
+  ac.sendGoal(goal);
+  
+  ac.waitForResult();
+  
+  if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+    ROS_INFO("Hooray, the robot reached the pickup zone!");
+  else
+    ROS_INFO("The base failed to move.");
+    
+  ros::Duration(5.0).sleep();
+
+  goal.target_pose.pose.position.y = -1.0;
+  goal.target_pose.pose.orientation.w = 1.0;
+  
+  ROS_INFO("Sending goal");
+  ac.sendGoal(goal);
+  
+  ac.waitForResult();
+  
+  if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+    ROS_INFO("Hooray, the robot reached the dropoff zone!");
+  else
+    ROS_INFO("The base failed to move.");
+  
+  return 0;
+}
--- src/scripts/pick_objects.sh
+++ src/scripts/pick_objects.sh
@@ -3,5 +3,6 @@ xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/wo
 sleep 5
 xterm -e "roslaunch turtlebot_gazebo amcl_demo.launch" &
 sleep 5
-xterm -e "roslaunch turtlebot_rviz_launchers view_navigation.launch"
+xterm -e "roslaunch turtlebot_rviz_launchers view_navigation.launch" &
 sleep 5
+xterm -e "rosrun pick_objects pick_objects"
