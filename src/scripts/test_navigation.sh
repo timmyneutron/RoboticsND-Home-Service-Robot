12a8d96000b113e2703245540dba8f62820b0ed5
Tim Abrahamsen <timmy_neutron@mac.com>
2019-04-26 19:03:28 +0000

    feat: Add map
    Add map for localization

--- src/scripts/test_navigation.sh
+++ src/scripts/test_navigation.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/RoboticsND-Home-Service-Robot/src/worlds/timsWorld.world " &
 sleep 5
-xterm -e "roslaunch turtlebot_gazebo amcl_demo.launch" &
+xterm -e "roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/workspace/RoboticsND-Home-Service-Robot/src/maps/map.yaml" &
 sleep 5
 xterm -e "roslaunch turtlebot_rviz_launchers view_navigation.launch"
 sleep 5
